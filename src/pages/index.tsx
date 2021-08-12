752d196284098ff67efc82f8cce14d0f1db4af49
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-08-12 18:53:38 -0300

    feat: added components and retrieving data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,20 @@ import styles from './home.module.scss'
 import axios from 'axios';
 import { useEffect } from 'react';
 import { GetStaticProps } from 'next';
+import { SearchBar } from '../components/SearchBar';
+import { WeatherCard } from '../components/WeatherCard';
 
 
+export default function Home({ weatherData }) {
 
-export default function Home({ data }) {
-
-  console.log(data)
 
   return (
     <div className={styles.mainContainer}>
       <span>NX-Weather</span>
-      
+      <div className={styles.contentContainer}>
+        <SearchBar />
+        <WeatherCard city={weatherData.city} weatherInfo={weatherData.weatherInfo} />
+      </div>
     </div>
   )
 }
@@ -22,10 +25,28 @@ export default function Home({ data }) {
 export const getStaticProps: GetStaticProps = async() => {
 
   const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Diadema&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric `)
-  const data = await res.data;
+
+  // const data = await res.data;
+
+  const weatherData = {
+    city: 'Diadema',
+    weatherInfo: {
+      description: res.data.weather.map(data=>{return data.description}).pop(),
+      temperature: res.data.main.temp,
+      minTemperature: res.data.main.temp_min,
+      maxTemperature: res.data.main.temp_max,
+      humidity: res.data.main.humidity,
+    }
+    
+
+  } 
+
+
 
   return {
-    props: {data}
+    props: {
+      weatherData
+    }
   }
 
 }
