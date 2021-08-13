5a8bb2e271d5a700092ac47fe8c96a6548849fd6
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-08-13 19:42:39 -0300

    fix: autocomplete with downshift

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,13 +1,61 @@
 
 import styles from './styles.module.scss'
 
+
+import { useCombobox } from 'downshift'
+
+import { useState } from 'react'
+
+const items = [
+  'teste',
+
+]
+
 export function SearchBar() {
 
-  return (
-    <div className={styles.searchContainer}>
-      <input type="text" placeholder="Search ...">
-      </input>
-    </div>
-  )
 
-} 
+    const [inputItems, setInputItems] = useState(items)
+    const {
+      isOpen,
+      getToggleButtonProps,
+      getLabelProps,
+      getMenuProps,
+      getInputProps,
+      getComboboxProps,
+      highlightedIndex,
+      getItemProps,
+    } = useCombobox({
+      items: inputItems,
+      onInputValueChange: ({ inputValue }) => {
+        setInputItems(
+          items.filter(item =>
+            item.toLowerCase().startsWith(inputValue.toLowerCase()),
+          ),
+        )
+      },
+    })
+    return (
+      <div className={styles.searchContainer}>
+        <div  {...getComboboxProps()}>
+          <input {...getInputProps()} placeholder="Search ..." />
+        </div>
+        <ul {...getMenuProps()}>
+          {isOpen &&
+            inputItems.map((item, index) => (
+              <li
+                style={
+                  highlightedIndex === index
+                    ? { backgroundColor: '#bde4ff' }
+                    : {}
+                }
+                key={`${item}${index}`}
+                {...getItemProps({ item, index })}
+              >
+                {item}
+              </li>
+            ))}
+        </ul>
+      </div>
+    )
+  }
+ 
