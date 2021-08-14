7c30580fcaff3972d61d9e4c29b32bbeb81dae2b
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-08-14 19:14:19 -0300

    fix: added auto sugestion to searchbar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,61 +1,112 @@
 
 import styles from './styles.module.scss'
-
-
+import cityDataList from '../../../city.list.json';
 import { useCombobox } from 'downshift'
+import {useVirtual} from 'react-virtual'
 
-import { useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 
-const items = [
-  'teste',
 
-]
+
+const dataListMap = Object.fromEntries(
+  Object.entries(cityDataList)
+  .map(([ val ]) => { return [val.name]})
+);
+const data = cityDataList;
+
+const cityList = data.map(val=>{return val.name})
+
+
+
+
+
+function getItems(search) {
+  return cityList.filter(n => n.toLowerCase().includes(search))
+}
 
 export function SearchBar() {
 
-
-    const [inputItems, setInputItems] = useState(items)
+    const [inputValue, setInputValue] = useState('')
+    const items = getItems(inputValue)
+    const listRef = useRef()
+    const rowVirtualizer = useVirtual({
+      size: 10,
+      parentRef: listRef,
+      estimateSize: useCallback(() => 30, []),
+      overscan: 2,
+    })
     const {
-      isOpen,
-      getToggleButtonProps,
+      getInputProps,
+      getItemProps,
       getLabelProps,
       getMenuProps,
-      getInputProps,
-      getComboboxProps,
       highlightedIndex,
-      getItemProps,
+      selectedItem,
+      getComboboxProps,
+      isOpen,
     } = useCombobox({
-      items: inputItems,
-      onInputValueChange: ({ inputValue }) => {
-        setInputItems(
-          items.filter(item =>
-            item.toLowerCase().startsWith(inputValue.toLowerCase()),
-          ),
-        )
-      },
+      items,
+      inputValue,
+      onInputValueChange: ({ inputValue: newValue }) =>
+        setInputValue(newValue),
+      scrollIntoView: () => {},
+      onHighlightedIndexChange: ({ highlightedIndex }) =>
+        rowVirtualizer.scrollToIndex(highlightedIndex),
     })
     return (
       <div className={styles.searchContainer}>
-        <div  {...getComboboxProps()}>
-          <input {...getInputProps()} placeholder="Search ..." />
+
+        <div {...getComboboxProps()}>
+          <input {...getInputProps({ type: 'text' })} />
         </div>
-        <ul {...getMenuProps()}>
-          {isOpen &&
-            inputItems.map((item, index) => (
+
+        <ul
+          {...getMenuProps({
+            ref: listRef,
+          })}
+        >
+          {isOpen && (
+            <>
               <li
-                style={
-                  highlightedIndex === index
-                    ? { backgroundColor: '#bde4ff' }
-                    : {}
-                }
-                key={`${item}${index}`}
-                {...getItemProps({ item, index })}
-              >
-                {item}
-              </li>
-            ))}
+                key="total-size"
+                // style={{ height: rowVirtualizer.totalSize }}
+              />
+              {rowVirtualizer.virtualItems.map(virtualRow => (
+                <li
+                  
+                  // key={items[virtualRow.index].id}
+                  {...getItemProps({
+                    index: virtualRow.index,
+                    item: items[virtualRow.index],
+                    // style: {
+                    //   backgroundColor:
+                    //     highlightedIndex === virtualRow.index
+                    //       ? 'lightgray'
+                    //       : 'inherit',
+                    //   fontWeight:
+                    //     selectedItem &&
+                    //     selectedItem.id === items[virtualRow.index].id
+                    //       ? 'bold'
+                    //       : 'normal',
+                    //   position: 'absolute',
+                    //   top: 0,
+                    //   left: 0,
+                    //   width: '100%',
+                    //   height: virtualRow.size,
+                    //   transform: `translateY(${virtualRow.start}px)`,
+                    // },
+                  })}
+                >
+                  {items[virtualRow.index]}
+                </li>
+              ))}
+            </>
+          )}
         </ul>
       </div>
+
+
+
     )
   }
  
