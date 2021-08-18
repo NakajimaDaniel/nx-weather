e2dbcdd28adf81f2f983436bc4a2fdb3baf9cca7
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-08-18 20:04:06 -0300

    fix: changed api call and show forecast data

--- src/pages/city/[id].tsx
+++ src/pages/city/[id].tsx
@@ -8,7 +8,7 @@ import styles from './cityWeather.module.scss';
 
 
 interface cityWeatherProps {
-  weatherData: {
+  weatherDataCurrent: {
     city: string,
     description: string,
     temperature: number,
@@ -17,32 +17,24 @@ interface cityWeatherProps {
     humidity: number,
   },
 
-  weatherForecastData: {
-    city: {
-      name: string,
-      country: string,
-    },
-    list: Array<weatherForecastUnit>
-  }
+  weatherForecastData: Array<weatherForecastUnit>
+  
 }
 
 interface weatherForecastUnit { 
   dt: number,
-  dt_txt: string,
-  weather: {
-    temp: number,
-    feelsLike: number,
-    minTemp: number,
-    maxTemp: number,
-    pressure: number,
-    humidity: number,
-    status: string,
-    description: string,
-    icon: string,
-  }
+  clouds: number,
+  humidity: number,
+  pressure: number,
+  tempDay: number,
+  tempMin: number,
+  tempMax: number,
+  weatherMain: string,
+  weatherDescription: string,
+  icon: string,
 }
 
-export default function CityWeather({weatherData, weatherForecastData}: cityWeatherProps) {
+export default function CityWeather({weatherForecastData,weatherDataCurrent }:cityWeatherProps) {
 
   const router = useRouter();
 
@@ -53,33 +45,53 @@ export default function CityWeather({weatherData, weatherForecastData}: cityWeat
     )
   }
 
-  console.log(weatherForecastData)
-  console.log(weatherData)
 
   return (
     <div className={styles.cityWeatherContainer}>
       <SearchBar />
-      <WeatherCard weatherInfo={weatherData} />
+      <WeatherCard weatherInfo={weatherDataCurrent} />
 
       <div className={styles.tomorrowWeather}>
         <div>
           <span>Weather for tomorrow</span>
-          <h1>{weatherForecastData?.list[0][0].weather?.temp}ºC</h1>
+          <h1>{weatherForecastData[0].tempDay}ºC</h1>
         </div>
 
         <div className={styles.tomorrowWeatherData}>
           <div>
-            <span>{weatherForecastData?.list[0][0].weather.minTemp}/{weatherForecastData?.list[0][0].weather.maxTemp}°C</span>
+            <span>{weatherForecastData[0].tempMin}/{weatherForecastData[0].tempMax}°C</span>
           </div>
           <div>
-            <span>{weatherForecastData?.list[0][0].weather.pressure}hPa</span>
+            <span>{weatherForecastData[0].pressure}hPa</span>
           </div>
           <div>
-            <span>{weatherForecastData?.list[0][0].weather.humidity}%</span>
+            <span>{weatherForecastData[0].humidity}%</span>
           </div>
         </div>
       </div>
-      
+
+      <div className={styles.forecastContainer}>
+        <p>Forecast for the next 5 days</p>
+
+        <div className={styles.forecastWrapper}>
+
+
+          {weatherForecastData.slice(0,5).map(data=> {
+            return (
+            <div className={styles.forecastUnit}>
+              <p>{data.dt}</p>
+              <span>{data.tempDay}°C</span>
+              <span>{data.tempMin}/{data.tempMax}°C</span>
+            </div>
+            )
+          })}  
+
+
+        </div>
+
+      </div>
+
+
     </div>
   )
 }
@@ -87,53 +99,50 @@ export default function CityWeather({weatherData, weatherForecastData}: cityWeat
 
 export const getServerSideProps = async ({params}) => {
 
-
-  const res = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=${params.id}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric `);
+  const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?id=${params.id}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric `);
   const data = await res.data;
 
-  const res2 = await axios.get(`http://api.openweathermap.org/data/2.5/weather?id=${params.id}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric `);
-  const data2 = await res2.data;
+  const lat = res.data.coord.lat;
+  const lon = res.data.coord.lon;
 
 
-  const weatherForecastData = {
-    city: {
-      name: res.data.city.name,
-      country: res.data.city.country,
-    },
-    list: [ res.data.list.map(data => {
-      return {
-        dt: data.dt,
-        dt_txt: data.dt_txt,
-        weather: {
-          temp: data.main.temp,
-          feelsLike: data.main.feels_like,
-          minTemp: data.main.temp_min,
-          maxTemp: data.main.temp_max,
-          pressure: data.main.pressure,
-          humidity: data.main.humidity,
-          status: data.weather[0].main,
-          description: data.weather[0].description,
-          icon: data.weather[0].icon,
-        }
-      }
-    })]
-
-  }
-
-  const weatherData = {
-    city: res2.data.name,
-    description: res2.data.weather.map(data=>{return data.description}).pop(),
-    temperature: res2.data.main.temp,
-    minTemperature: res2.data.main.temp_min,
-    maxTemperature: res2.data.main.temp_max,
-    humidity: res2.data.main.humidity,
+  const weatherDataCurrent = {
+    city: res.data.name,
+    description: res.data.weather.map(data=>{return data.description}).pop(),
+    temperature: res.data.main.temp,
+    minTemperature: res.data.main.temp_min,
+    maxTemperature: res.data.main.temp_max,
+    humidity: res.data.main.humidity,
     
   }
 
+  const res2 = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric `)
+  const data2 = res2.data;
+
+
+  const weatherForecastData = 
+
+    res2.data.daily.map(data => {
+      return {
+        dt: data.dt,
+        clouds: data.clouds,
+        humidity: data.humidity,
+        pressure: data.pressure,
+        tempDay: data.temp.day,
+        tempMin: data.temp.min,
+        tempMax: data.temp.max,
+        weatherMain: data.weather[0].main,
+        weatherDescription: data.weather[0].description,
+        icon: data.weather[0].icon,
+      }
+    })
+
+  
+
   return {
     props: {
-      weatherData,
-      weatherForecastData
+      weatherForecastData,
+      weatherDataCurrent
     }
   }
 
