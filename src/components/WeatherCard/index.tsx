0f5cf487792812f22486c9790c6734ff87adac9c
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-08-25 19:24:27 -0300

    fix: stylization adjs

--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -10,27 +10,35 @@ type WeatherCardProps = {
     minTemperature: number,
     maxTemperature: number,
     humidity: number,
-  }
+    icon: string,
+    country: string,
+    dt: number,
+    timezone: number,
+  },
+  isNight: boolean,
 
 }
 
 
-export function WeatherCard({weatherInfo}: WeatherCardProps) {
+export function WeatherCard({weatherInfo, isNight}: WeatherCardProps) {
 
-  console.log(weatherInfo)
+  const weatherDate = new Date((weatherInfo.dt * 1000) + (weatherInfo.timezone * 1000)).toUTCString();
+
+
 
   return (
-    <div className={styles.cardContainer}>
+    <div className={isNight?  styles.cardContainer : styles.cardContainerDay}>
       <div className={styles.cardContent}>
         <div className={styles.cardContentMainInfo}>
-          <span>{weatherInfo.city}</span>
-          <h2>{weatherInfo.temperature}ºC</h2>
+          <span>Current weather in {weatherInfo.city}, {weatherInfo.country} </span>
+          <h2>{Math.round(weatherInfo.temperature * 10)/10}ºC</h2>
           <p>{weatherInfo.description}</p>
+          <h3>Updated at {weatherDate}</h3>
         </div>
 
         <div className={styles.cardContentSideInfo}>
-          <img src="/images/nights.svg" alt="moon" />
-          <span>{weatherInfo.minTemperature}/{weatherInfo.maxTemperature}ºC</span>
+          <img src={`http://openweathermap.org/img/wn/${weatherInfo.icon}@2x.png`} />
+          <span>{Math.round(weatherInfo.minTemperature * 10) / 10}/{Math.round(weatherInfo.maxTemperature * 10) / 10 }ºC</span>
         </div>
       </div>
     </div>
