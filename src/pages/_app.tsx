549e25efb9a7cd3350f92c7f9b600253dd1e49dd
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-08-26 19:11:19 -0300

    weather icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
-import '../styles/global.scss'
+import '../styles/global.scss';
+import '../styles/css/weather-icons.min.css';
+import '../styles/css/weather-icons-wind.min.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
