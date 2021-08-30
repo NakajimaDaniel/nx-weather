070cf9d36ea1a1feb72c44f9addd849caa8e3e0a
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-08-30 19:27:37 -0300

    feat: show cities weather card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,44 @@
 import styles from './home.module.scss'
 
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { GetStaticProps } from 'next';
-import { SearchBar } from '../components/SearchBar';
+
 import { WeatherCard } from '../components/WeatherCard';
+import { Header } from '../components/Header';
 
 
 export default function Home({ weatherData }) {
 
+  const UTCHour = new Date((weatherData[0].dt * 1000) + (weatherData[0].timezone * 1000)).getUTCHours();
+
+  const [isNight, setIsNight] = useState(false)
+
+
+
+  useEffect(() => {
+    if(UTCHour >= 18 || UTCHour>= 0 && UTCHour <= 5) {
+      setIsNight(true);
+    } else {
+      setIsNight(false);
+    }
+  }, [UTCHour])
+
 
   return (
     <div className={styles.mainContainer}>
-      <span>NX-Weather</span>
+      <Header /> 
+        
       <div className={styles.contentContainer}>
-        <SearchBar />
-        <WeatherCard weatherInfo={weatherData} />
+
+        {weatherData.map(data => {
+          return(
+          <WeatherCard weatherInfo={data} isNight={isNight} />
+        )})}
+      
       </div>
+
+
     </div>
   )
 }
@@ -24,21 +46,25 @@ export default function Home({ weatherData }) {
 
 export const getStaticProps: GetStaticProps = async() => {
 
-  const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Diadema&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric `)
 
-  // const data = await res.data;
+  const res = await axios.get(`http://api.openweathermap.org/data/2.5/find?lat=-23.550520&lon=-46.633308&cnt=6&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric`)
 
-  const weatherData = {
-    city: 'Diadema',
-    description: res.data.weather.map(data=>{return data.description}).pop(),
-    temperature: res.data.main.temp,
-    minTemperature: res.data.main.temp_min,
-    maxTemperature: res.data.main.temp_max,
-    humidity: res.data.main.humidity,
+  const data = res.data;
+
+  const weatherData = data.list.map(data => {
+    return{
+      city: data.name,
+      description: data.weather.map(data=>{return data.description}).pop(),
+      temperature: data.main.temp,
+      minTemperature: data.main.temp_min,
+      maxTemperature: data.main.temp_max,
+      humidity: data.main.humidity,
+      dt: data.dt,
+      icon: data.weather.map(data => {return data.icon}).pop(),
+      country: data.sys.country,
+      timezone: -10800,
     }
-    
-
-
+  });
 
 
   return {
