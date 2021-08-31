a7e0fff45097d68fc2cf13dbb50fe0f5ecb8bb05
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-08-31 20:22:07 -0300

    added spinner css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/global.scss';
 import '../styles/css/weather-icons.min.css';
 import '../styles/css/weather-icons-wind.min.css';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
