0eee82a801af178688c1dfcb2375a5439f691c8c
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-09-05 17:45:12 -0300

    jest and testing library initial config

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules", "/.next/"],
+  setupFilesAfterEnv: [
+    "<rootDir>/src/tests/setupTests.ts"
+  ],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  testEnvironment: 'jsdom',
+
+  moduleNameMapper: {
+    // "\\.(scss|css|sass)$": "identity-obj-proxy",
+
+     /* Handle CSS imports (with CSS modules)
+    https://jestjs.io/docs/webpack#mocking-css-modules */
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+    /* Handle image imports
+    https://jestjs.io/docs/webpack#handling-static-assets */
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+
+};
+
