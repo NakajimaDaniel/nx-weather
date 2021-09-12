20b971870c8e27a5e63f2730f6e03235da6b41ef
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-09-12 17:45:02 -0300

    fix: link to home on img

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { SearchBar } from "../SearchBar";
 import styles from './styles.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function Header() {
   return (
     <div className={styles.HeaderContainer}>
-      <div className={styles.HeaderImage}>
-        <Image src="/assets/nx-weather-title.png"  width={160} height={26} />
-      </div>
-      
+      <Link href="/">
+        <div className={styles.HeaderImage}>
+          <Image src="/assets/nx-weather-title.png"  width={160} height={26} />
+        </div>
+      </Link>
       <SearchBar />
     </div>
   )
