5bf106b3274e04cb5b5a953fb927f56b3c0ada38
NakajimaDaniel <nakajimadaniel96@gmail.com>
2023-03-22 19:48:19 -0300

    test if clear the error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 
 import type { AppProps } from 'next/app'
 import "../styles/global.css";
+/*
 import { Noto_Sans } from 'next/font/google'
 
 const NotoSans = Noto_Sans({
@@ -8,7 +9,7 @@ const NotoSans = Noto_Sans({
   style: ['normal', 'italic'],
   subsets: ['latin'],
   variable:'--font-noto'
-})
+}) */
 
 
 export default function App({ Component, pageProps }: AppProps) {
