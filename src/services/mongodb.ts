c41e4371936c5807b4c677016e1236106b5711df
NakajimaDaniel <nakajimadaniel96@gmail.com>
2023-03-22 20:08:20 -0300

    fixed mongodb env

--- src/services/mongodb.ts
+++ src/services/mongodb.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb'
 
-const uri = process.env.ATLAS_URI
-
+//const uri = process.env.ATLAS_URI
+const uri = process.env.MONGODB_URI
 
 const client = new MongoClient(uri);
 
